f98434d803af02a0d71f5aaf648224c99578d9b7
Olausz <2023211510080@iesp.edu.br>
2025-03-07 11:12:47 -0300

    Corrigindo Produto

--- Estudos/Curso Java Udemy/Sessao13/src/ExercicioFixacao/Entities/UsedProduct.java
+++ Estudos/Curso Java Udemy/Sessao13/src/ExercicioFixacao/Entities/UsedProduct.java
@@ -3,7 +3,7 @@ package ExercicioFixacao.Entities;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public final class UsedProduct extends Product{
+public final class UsedProduct extends Product {
 
     private Date manufactureDate;
 
@@ -15,12 +15,17 @@ public final class UsedProduct extends Product{
     public Date getManufactureDate() {
         return manufactureDate;
     }
+
     public void setManufactureDate(Date manufactureDate) {
         this.manufactureDate = manufactureDate;
     }
 
     @Override
     public String priceTag() {
-    return getPrice() + " (used)" + " $ " + getPrice() + "(Manufacture date: "+ String.valueOf(manufactureDate) + ")";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String formattedDate = sdf.format(manufactureDate);
+
+        return getName() + " (used) $" + String.format("%.2f", getPrice()) + " (Manufacture date: " + formattedDate + ")";
     }
 }
